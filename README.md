bd5d1ef154c7cc7ffd5e98de469383bcc38e8f7f
kdchang <kdchang.cc@gmail.com>
2016-11-10 21:20:22 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# JeibaChart 
+A online Chinese text segmentation visualization tool based Jeiba.
+
+## Usage 
+
+Setup virtualenv
+
+```
+$ virtualenv -m python3 python-jieba-chart-venv
+```
+
+```
+$ source/python-jieba-chart-venv/bin/activate
+```
+
+```
+$ git clone 
+```
---
+++ src/scripts/main.js
@@ -0,0 +1,49 @@
+import Chart from 'chart.js'
+
+const REQUEST_URL = './api/count.json';
+let data = '';
+let str = '';
+
+function fetchData() {
+    fetch(REQUEST_URL).then((response) => {
+        return response.json();
+    }).then(function(json) {
+      data = json;
+      loadChart(data);
+    });
+}
+
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function loadChart(rawData) {
+    const ctxRef = document.querySelector('#data-chart');
+    let labels = [];
+    let counts = [];
+    let colors = [];
+    for(let i = 0; i <= 30; i++) {
+      labels.push(rawData[i][0]);
+      counts.push(rawData[i][1]);
+      colors.push(getRandomColor())      
+    }
+
+    const data = {
+        labels: labels,
+        datasets: [{
+            data: counts,
+            backgroundColor: colors,
+        }]
+    };
+    const myPieChart = new Chart(ctxRef, {
+    type: 'pie',
+        data: data,
+    });      
+}
+
+fetchData();
